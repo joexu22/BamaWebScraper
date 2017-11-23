e6cde77c8293d794760fd119471baeba166ed413
Joe Xu <gxu2@crimson.ua.edu>
2017-11-23 03:17:47 -0500

    scrolling through the state list

--- BamaWebScrape.py
+++ BamaWebScrape.py
@@ -1,20 +1,54 @@
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
-from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# Creating a instance of Chrome driver
+# creating a instance of Chrome driver
 driver = webdriver.Chrome("./chromedriver")
-driver.get("http://www.google.com")
+
+# starting up an instance of the website
+# TODO: have this in config file
+# TODO: wondering if its better to have this starting point be google
+driver.get("https://ssb.ua.edu/pls/PROD/rtstreq.P_Searchtype")
+
+# this suppose is used to manually check right now
 print(driver.title)
-inputElement = driver.find_element_by_name("q")
-inputElement.send_keys("cheese!")
-inputElement.submit()
 
-try:
+# selecting radio-button
+radio_button_css = "input[type='radio'][value='BAMANAME']"
+bamaRadioBtn = driver.find_element_by_css_selector(radio_button_css)
+bamaRadioBtn.click()
+
+# hitting search
+# TODO: a chance to practice OO programming here
+search_button_css = "input[type='submit'][value='Submit']"
+bamaSearchBtn = driver.find_element_by_css_selector(search_button_css)
+bamaSearchBtn.click()
+
+# checking... hopefully Bama doesn't change their titles
+print(driver.title)
+WebDriverWait(driver, 600)
+
+# checking...
+bamaState = Select(driver.find_element_by_id("p_state"))
+#print([state.text for state in bamaState.options])
+
+# look at the wheel go
+# TODO: not mess around...
+for state in bamaState.options:
+    bamaState.select_by_visible_text(state.text)
+    WebDriverWait(driver, 60)
+
+driver.quit()
+#inputElement = driver.find_element_by_name("q")
+#inputElement.send_keys("cheese!")
+#inputElement.submit()
+
+#try:
     #WebDriverWait(driver, 10).until(EC.title_contains("cheese!"))
-    WebDriverWait(driver, 10)
-    print(driver.title)
+    #WebDriverWait(driver, 60)
+    #print(driver.title)
 
-finally:
-    driver.quit()
+#finally:
+    #driver.quit()
